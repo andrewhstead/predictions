75a0321958f0d741cf3f1cbef82dca50e781f293
Andrew Stead <andrew@andrewstead.co.uk>
2018-08-20 11:35:46 +0100

    Initial Commit

---
+++ home/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from users.models import User
+
+# Create your views here.
+# The default home page view.
+def home_page(request):
+
+    users = User.objects.all()
+    
+    return render(request, "home.html", {"users": users})
